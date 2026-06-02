602823b3473daca76629278e482a4cf4778f8269
saumyasinghcs28-commits <saumya.singh.cs28@iilm.edu>
2026-06-02 15:32:24 +0530

    xsbsq

--- Saumya/Hi.py
+++ Saumya/Hi.py
@@ -1,4 +1,4 @@
 print("Hi, I am Saumya")
 print("I am learning Python")
 print("I am enjoying it") 
-print ("adarsh")
+jrf
---
+++ Saumya/day1/calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextint();
+int b = sc.nextint();
+System.out.println("Addition =" +(a+b));
+System.out.println("substraction =" +(a-b));
+System.out.println("multiplication =" +(a*b));
+System.out.println("division =" +(a/b));
+}
