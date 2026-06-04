9f46f2ddc145a6ddf6bb47b8d9ef16a75af20bf9
Kriti Keshri <kritisocial806@gmail.com>
2026-06-04 17:57:57 +0530

    DSA Day 4 done

---
+++ Kriti/DSA_Day_4/Power_of_no.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int power(int a, int n)
+{
+    if(n == 0)
+        return 1;
+
+    return a * power(a, n - 1);
+}
+
+int main()
+{
+    int a, n;
+
+    cout << "Enter base: ";
+    cin >> a;
+
+    cout << "Enter power: ";
+    cin >> n;
+
+    cout << "Answer = " << power(a, n);
+
+    return 0;
+}
